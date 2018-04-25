ec4bbb9dcb9cc0cf54d2ee07d5942eb3e8890c7a
Brix Angeles <brix.angeles@gmail.com>
2018-04-24 21:42:01 -0700

    single page app done

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function Person (props) {
-    return (
-      <div className="person">
-      <h1>{props.name}</h1>
-      <p>Number is: {props.number}</p>
-    </div>
-    );
-  }
-  
-  ReactDOM.render(<Person name="Brix" number="9"/>, document.querySelector("#p1"));
-  
-  ReactDOM.render(<Person name="Brielle" number="17"/>, document.querySelector("#p2"));
+  return (
+    <div className="person">
+    <h1>{props.name}</h1>
+    <p>Number is: {props.number}</p>
+  </div>
+  );
+}
+
+let app = (
+  <div>
+    <Person name="Brix" number="9"/>
+    <Person name="Brielle" number="17"/>
+  </div>
+);
+
+ReactDOM.render(app, document.querySelector("#app"));
