11306a6a987cf535276fbb5f144d91744e8fe20f
Brix <brienyll29@gmail.com>
2018-08-21 06:47:55 -0700

    Create adv-arrays-ex.js

---
+++ adv-array/adv-arrays-ex.js
@@ -0,0 +1,35 @@
+// Complete the below questions using this array:
+const array = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  },
+
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+let newArray = [];
+const exArray = array.forEach(user => {
+  let {username} = user;
+  newArray.push(`${username}!`);
+});
