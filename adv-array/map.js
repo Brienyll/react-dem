233c7231f52f4beb24f0180695f46c9b7137382f
Brix <brienyll29@gmail.com>
2018-08-21 06:26:32 -0700

    cleaned code

--- adv-array/map.js
+++ adv-array/map.js
@@ -1,7 +1,5 @@
 const array = [1, 2, 5, 8];
 
-const mapArray = array.map((num) => {
-  return num * 2;
-});
+const mapArray = array.map(num) => return num * 2
 
-console.log (mapArray);
+console.log(mapArray);
