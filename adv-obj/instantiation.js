8d24c473619f5f64d30516acf4d8f2203ffcd840
Brix <brienyll29@gmail.com>
2018-08-22 00:31:04 -0700

    Create instantiation.js

---
+++ adv-obj/instantiation.js
@@ -0,0 +1,22 @@
+class Player {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+    console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    }
+}
+
+class Wizard extends Player {
+    constructor(name, type) {
+        super(name, type)
+    }
+    play() {
+        console.log(`I'm a ${this.type}`);
+    }
+}
+
+const wizard1 = new Wizard("Shelly", "Healer");
+
+const wizard3 = new Wizard("Shawn", "Dark Magician");
