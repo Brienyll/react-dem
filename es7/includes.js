52566b375e226c8b3af1b4b878eab609fac763ba
Brix <brienyll29@gmail.com>
2018-08-22 00:39:37 -0700

    Create includes.js

---
+++ es7/includes.js
@@ -0,0 +1,5 @@
+Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.filter(name => name.includes('John')) // ['Johnathan']
