cc32f75ac0cbfdcd578327be4115cbb49cc66b84
Brix <brienyll29@gmail.com>
2018-08-23 01:06:01 -0700

    Update objects.js

--- es8/objects.js
+++ es8/objects.js
@@ -13,3 +13,23 @@ user0 Santa
 user1 Rudolph
 user2 Grinch
 */
+
+Object.values(obj).forEach((value => {
+  console.log(value)
+})
+
+/*
+Santa
+Rudolph
+Grinch
+*/
+
+Object.entries(obj).forEach(value => {
+  console.log(value)
+})
+
+/*
+[ 'user0', 'Santa' ]
+[ 'user1', 'Rudolph' ]
+[ 'user2', 'Grinch' ]
+*/
